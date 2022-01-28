6c992ee2f45d372394bbdc3d32cf611e392cc5f6
Đỗ Xuân Nam <namnumbernine@gmail.com>
2022-01-28 22:56:24 +0700

    28/1/2022

---
+++ resources/views/about.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.default')
+
+@section('content')
+    <section class="h-screen flex items-center">
+    <div class="container mx-auto">
+        <h1 class="text-white text-8xl mb-6 uppercase font-black">
+            About my website
+        </h1>
+        <p class="text-white text-lg font-light tracking-wider mb-6">
+            It is my first time i use Laravel to code my first website
+            <br>
+            So i so confuse :)
+        </p>
+        <a href="/" class="text-purple-500">Go Home</a>
+    </div>
+    </section>
+@endsection
---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.default')
+
+@section('content')
+    <section class="h-screen flex items-center">
+    <div class="container mx-auto">
+        <h1 class="text-white text-8xl mb-6 uppercase font-black">
+            Nam <span class ="text-purple-500">Xuân</span>
+        </h1>
+        <h3 class="text-white text-4xl font-light tracking-wider">
+            Welcome to my website, it my first time with Laravel!
+        </h3>
+    </div>
+    </section>
+@endsection
